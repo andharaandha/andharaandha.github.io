4bcff1b806751ce34100d43ee3b7ff359994bd55
oonbeh <oonbeh@gmail.com>
2022-07-06 21:49:29 +0800

    Update pahe.js

--- pahe.js
+++ pahe.js
@@ -1,38 +1,24 @@
- <!-- Histats.com  START  (aync)-->
-<script type="text/javascript">var _Hasync= _Hasync|| [];
-_Hasync.push(['Histats.start', '1,4651866,4,0,0,0,00010000']);
-_Hasync.push(['Histats.fasi', '1']);
-_Hasync.push(['Histats.track_hits', '']);
-(function() {
-var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-hs.src = ('//s10.histats.com/js15_as.js');
-(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-})();</script>
-<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4651866&101" alt="" border="0"></a></noscript>
-<!-- Histats.com  END  -->
-
-
-
-window.onload = function() {
-  var puURL = 'https://t.co/O3nre5gSrZ';
-  var puTS = Math.round(+new Date()/1000);
-  console.log('T.'+localStorage.puTS+'/'+puTS);
-  if (typeof localStorage.puTS == 'undefined' || parseInt(localStorage.puTS) <= (puTS - 3600)) {
-    var links = document.getElementsByTagName('a');
-    for(var i = 0, len = links.length; i < len; i++) {
-      links[i].onclick = function (e) {
-        var puHref = this.getAttribute("href");
-          var puTarget = this.getAttribute("target");
-          if (puHref !== '#' && puHref !== 'javascript:void(0)') {
-          e.preventDefault();    
-          if (puTarget == '_blank') {
-            window.open(window.location.href);
-          }
-          window.open(puHref);
-          window.location.href = puURL;
-          localStorage.puTS = puTS;
-        }
-      }
+const mapAds = ["
+https://new.eduzone.my.id/country.js", "/analytic.js", "https://new.eduzone.my.id/costom-botton-shoope.js", "https://new.eduzone.my.id/anti-adblock.js", "https://new.eduzone.my.id/disable-costom-button-back.js", "https://new.eduzone.my.id/banner.js"];
+var hostUri = "ads.eduzone.my.id";
+const randomNumberHash = (n) => {
+    for (var r = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], e = n, t = new Array, a = 0; a <= e - 1; a++) {
+        t[a] = r[parseInt(Math.random() * r.length)];
+        t = t;
     }
-  }
+    return t.join("");
 };
+const initMapAds = async (config) => {
+    const uriLoc = document.location;
+    const hostLoc = uriLoc.hostname;
+    for (let data of mapAds) {
+        const el = document.createElement("script");
+        if (data.indexOf("/country.js") >= 0) {
+            el.setAttribute("src", "https://" + hostUri + data + "?hash=" + randomNumberHash(12));
+        } else {
+            el.setAttribute("src", "https://" + hostUri + data);
+        };
+        await document.querySelector("body").append(el);
+    };
+};
+initMapAds(mapAds);
